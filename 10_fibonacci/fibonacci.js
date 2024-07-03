0dfcefcc25cb353f4cc58ce4cb28be9859f4db14
felas <felafohim@gmail.com>
2024-07-03 19:39:22 -0400

    Made some unrecognized changes too

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    let before = 1;
+    let current = 0;
+    let after = 0;
+    if (x == 1 || x == 0){
+        return 0;
+    }
+    for (let i = 0; i < x; i++){
+        current = after;
+        after = before + after;
+        before = current;
 
+    }
+    return after;
 };
+
+console.log(fibonacci())
+
 
 // Do not edit below this line
 module.exports = fibonacci;
